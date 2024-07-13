8d2536866d6c8318219033ea087568b567d87f1f
Lee_D <leegreat21@gmail.com>
2024-07-13 16:16:54 +0300

    render-messages

--- fire-chat/src/components/Chat.js
+++ fire-chat/src/components/Chat.js
@@ -1,13 +1,32 @@
-import { useState } from "react";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  serverTimestamp,
+  where,
+} from "firebase/firestore";
 import { auth, db } from "./../firebase-config";
+import "./Chat.css";
 
 const Chat = (props) => {
   const { room } = props;
   const [newMessages, setNewMessages] = useState("");
+  const [messages, setMessages] = useState([]);
 
   const messageRef = collection(db, "messages");
 
+  useEffect(() => {
+    const queryMessages = query(messageRef, where("room", "==", room));
+    onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+  }, []);
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (newMessages === "") return;
@@ -23,6 +42,11 @@ const Chat = (props) => {
   };
   return (
     <div className="chat-app">
+      <div>
+        {messages.map((message) => (
+          <h1>{message.text}</h1>
+        ))}
+      </div>
       <form onSubmit={handleSubmit} className="new-message-form">
         <input
           className="new-massage__input"
